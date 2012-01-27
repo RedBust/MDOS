dfddc46b16d3427ea26fbc9e573847d1ea9ed742
Red Bust <redbust@hotmail.fr>
2012-01-27 19:45:50 +0100

    Last commit todaaaaay ! Ya mother fucker !

--- task/connexions/connect.php
+++ task/connexions/connect.php
@@ -1,8 +1,9 @@
 <?php
 include dirname(__FILE__).'/../../api/core.php';
 include dirname(__FILE__).'/../../static/config.php';
+$MDOS = new MDOS;
 if ((isset ($_GET['lang'])) and (isset ($_GET['localgateway'])) and (isset($_GET['os'])) and (isset($_GET['macadr'])) and (isset($_GET['archetype'])) and (isset($_GET['pcname'])) and (isset ($_GET['ram'])) and (isset ($_GET['sid']))) {
-	if (MDOS::SecureCheckZombSID($_GET['sid'])) {
+	if ($MDOS -> SecureCheckZombSID($_GET['sid'])) {
 		$lang = $_GET['lang'];
 		$localgateway = $_GET['localgateway'];
 		$os = $_GET['os'];
@@ -10,18 +11,18 @@ if ((isset ($_GET['lang'])) and (isset ($_GET['localgateway'])) and (isset($_GET
 		$archetype = $_GET['archetype'];
 		$pcname = $_GET['pcname'];
 		$ram = $_GET['ram'];
-		$ip = (MDOS::RetrieveIP());
+		$ip = ($MDOS -> RetrieveIP());
 		$sid = $_GET['sid'];
 		_register ($ip, $lang, $localgateway, $os, $macadr, $archetype, $pcname, $ram, $ip, $sid);
 		exit;
 	}
 	else {
-		MDOS::WriteLog ((MDOS::RetrieveIP()), '#2', 'CSRF MDOS attack attempt. (zombie client can not give a bad secure connexion id.)');
+		$MDOS -> WriteLog (($MDOS -> RetrieveIP()), '#2', 'CSRF MDOS attack attempt. (zombie client can not give a bad secure connexion id.)');
 		exit;
 	}
 }
 else {
-	MDOS::WriteLog ((MDOS::RetrieveIP()), '#2', 'CSRF MDOS attack attempt. (zombie client can not make any registration error.)');
+	$MDOS -> WriteLog (($MDOS -> RetrieveIP()), '#2', 'CSRF MDOS attack attempt. (zombie client can not make any registration error.)');
 	exit;
 }
 
